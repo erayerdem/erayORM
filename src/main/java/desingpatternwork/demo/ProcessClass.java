5de239da5d69c619c1f02aa5a5ab0f4f0ba41696
samet eray erdem <sameterayerdm@gmail.com>
2018-11-08 11:06:05 +0300

    changed method

--- src/main/java/desingpatternwork/demo/ProcessClass.java
+++ src/main/java/desingpatternwork/demo/ProcessClass.java
@@ -1,42 +1,11 @@
 package desingpatternwork.demo;
 
 import java.lang.reflect.Field;
+import java.util.Date;
 
 public class ProcessClass {
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-    String c="INSERT INTO TABLE_NAME VALUES (value1,value2,value3,...valueN);\n";
-        Student student = new Student();
-        student.setAge(12);
-        student.setName("samet");
-        student.setSurname("eray erdem");
-
-        String trim=student.getClass().getName().toString().trim();
-
-
-        String myquery="INSERT INTO "+trim.substring(trim.lastIndexOf('.')+1,trim.length())+" (";
-        System.out.println(myquery);
-        Field[] declaredFields = student.getClass().getDeclaredFields();
-        for (Field field:declaredFields) {
-
-        int start=field.toString().trim().lastIndexOf(".")+1;
-         int end=field.toString().trim().length();
-            Field declaredField = student.getClass().
-                    getDeclaredField(field.toString().trim().substring(start,end));
-        declaredField.setAccessible(true);
-            Object o = declaredField.get(student);
-            myquery+=o;
-            myquery+=",";
-        }
-
-        System.out.println(myquery);
-
-        Field field = student.getClass().getDeclaredField("name");
-
-        field.setAccessible(true);
-
-        Object value = field.get(student);
-        Class<?> type = field.getType();
 
     }
 }
---
+++ src/main/java/desingpatternwork/demo/SqlRepository.java
@@ -0,0 +1,56 @@
+package desingpatternwork.demo;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+
+public class SqlRepository <T>{
+
+    public  void insertPojo(T t) throws IllegalAccessException, NoSuchFieldException {
+        String c="INSERT INTO TABLE_NAME VALUES (value1,value2,value3,...valueN);\n";
+        Student student = new Student();
+        student.setAge(12);
+        student.setName("samet");
+        student.setSurname("eray erdem");
+
+        String trim=student.getClass().getName().toString().trim();
+
+
+        String myquery="INSERT INTO "+trim.substring(trim.lastIndexOf('.')+1,trim.length())+" (";
+        System.out.println(myquery);
+        Field[] declaredFields = student.getClass().getDeclaredFields();
+        for (Field field:declaredFields) {
+
+            int start=field.toString().trim().lastIndexOf(".")+1;
+            int end=field.toString().trim().length();
+            Field declaredField = student.getClass().
+                    getDeclaredField(field.toString().trim().substring(start,end));
+            declaredField.setAccessible(true);
+            Object o = declaredField.get(student);
+            if(o instanceof  String || o instanceof Date) {
+                myquery+=" '";
+                myquery+=o;
+                myquery+=" '";
+            }
+            else {
+                myquery+=o;}
+            myquery+=",";
+        }
+
+        myquery=myquery.substring(0,myquery.length()-1);
+        myquery+=")";
+        System.out.println(myquery);
+
+        Field field = student.getClass().getDeclaredField("name");
+
+        field.setAccessible(true);
+
+        Object value = field.get(student);
+        Class<?> type = field.getType();
+    }
+
+    public  void createTable(){
+
+
+
+    }
+}
